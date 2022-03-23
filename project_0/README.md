c236197d89984e8ac43090f6d3c19b8b7af3b09f
MVulf <vulfjob@gmail.com>
2022-03-23 15:21:50 +0300

    Project_0. The fastest algorithm was found

--- project_0/README.md
+++ project_0/README.md
@@ -31,10 +31,11 @@ Necessary to write a program for guessing a random number for a minimum number o
 
 **Quality metric**
 
-Mean number of attempts in case of 1000 repeating.
+Mean number of attempts in case of 1000 repeating. Might be less then 20.
 
-**Practice**
-writing Python-code.
+**Practice:**
+- Writing Python-code according to PEP-8.
+- Working with IDE and GitHub.
 
 :arrow_up:[to content](https://github.com/mvulf/sf_data_science/tree/main/project_0/README.md#Content)
 
@@ -44,13 +45,35 @@ No external data.
 :arrow_up:[to content](https://github.com/mvulf/sf_data_science/tree/main/project_0/README.md#Content)
 
 ### Project stages
+1. Repository design.
+
+2. Base game kernel design.
+    
+    - Function decorator have been created for retrieving quality metric: mean number of attempts. There can be different values of test numbers.
+    - The game kernel has the possibility to get different range of guessing value. It's possible to set a random or manual value for guessing.
+    - The game kernel has different checks for the received values.
+ 
+3. Guess algorithm design.
+
 
 :arrow_up:[to content](https://github.com/mvulf/sf_data_science/tree/main/project_0/README.md#Content)
 
 ### Results
 
+Two algorithms with satisfied metrics have been created.
+
+- Algorithm 'random_predict_range_dividing' predicting the number with dividing search range according to "more/less" fedback.
+- Algorithm 'predict_division_two' predicting the number by getting an average value of range, which is decreasing according to "more/less" fedback.
+
+Both algorithms were compared with random method:
+- 'random_predict' guesses random numbers in range [1, 100] on average in 101 attempts. Total count of tests: 1000
+- 'random_predict_range_dividing' guesses random numbers in range [1, 100] on average in 9 attempts. Total count of tests: 1000
+- 'predict_division_two' guesses random numbers in range [1, 100] on average in 5 attempts. Total count of tests: 1000
+
 :arrow_up:[to content](https://github.com/mvulf/sf_data_science/tree/main/project_0/README.md#Content)
 
 ### Conclusion
+
+Algorithm 'predict_division_two' is the shortest method for guessing a random number in comparison with other introduced methods. It guesses random numbers in range [1, 100] on average in 5 attempts. Total count of tests was 1000.
 
 :arrow_up:[to content](https://github.com/mvulf/sf_data_science/tree/main/project_0/README.md#Content)
--- project_0/game_kernel.py
+++ project_0/game_kernel.py
@@ -8,15 +8,11 @@ Further it can be an attribute of game_kernel class.
 
 import numpy as np
 
-# Set up count of tests
-test_number = 1000
-
-
-def score_game(test_number:int=1) -> callable:
+def score_game(test_number:int=1000) -> callable:
     """Decorator for game kernels tests up to test_number times
 
     Args:
-        test_number (int): Number of necessary tests. Defaults to 1.
+        test_number (int): Number of necessary tests. Defaults to 1000.
 
     Returns:
         callable: Function for testing game kernel
@@ -25,13 +21,15 @@ def score_game(test_number:int=1) -> callable:
     # Check, that number of tests more then zero
     try:
         if test_number < 1:
-            raise ValueError("Test number might be more than zero. Set one test")
+            raise ValueError("Test number might be more than zero."
+                             + " Set one test")
     except ValueError as e:
         print(e)
         test_number = 1
     
     def test_kernel_decorator(test_kernel:callable) -> callable:
-        """Decorator for calculating mean number of retries in test_number cases
+        """Decorator for calculating mean number of retries in 
+        test_number cases
 
         Args:
             test_kernel (callable): Tested guess function
@@ -40,13 +38,18 @@ def score_game(test_number:int=1) -> callable:
             callable: Mean number of attempts
         """
         
-        def score_game_kernel(guess_number:int=None, min_val:int=1, max_val:int=100) -> int:
-            """Function for calculating mean number of attempts in test_number cases 
+        def score_game_kernel(guess_number:int=None, min_val:int=1, \
+            max_val:int=100) -> int:
+            """Function for calculating mean number of attempts in 
+            test_number cases 
 
             Args:
-                guess_number (int, optional): Number for guessing. Defaults to None.
-                min_val (int, optional): Minimum value of guess number. Defaults to 1.
-                max_val (int, optional): Maximum value of guess number. Defaults to 100.
+                guess_number (int, optional): Number for guessing. 
+                    Defaults to None.
+                min_val (int, optional): Minimum value of guess number. 
+                    Defaults to 1.
+                max_val (int, optional): Maximum value of guess number. 
+                    Defaults to 100.
 
             Returns:
                 int: Mean number of attempts
@@ -61,7 +64,8 @@ def score_game(test_number:int=1) -> callable:
             # Get array of guess numbers
             if guess_number is None:
                 np.random.seed(1) # Fix seed for repeatability
-                number_array = np.random.randint(min_val, max_val+1, size=(test_number))
+                number_array = np.random.randint(min_val, max_val+1, \
+                    size=(test_number))
                 if test_number == 1:
                     num_type_str = 'a random number'
                 else:
@@ -73,10 +77,22 @@ def score_game(test_number:int=1) -> callable:
             # Calculating mean count of attempts for test_number tests
             attempts_numbers = []
             for number in number_array:
-                attempts_numbers.append(test_kernel(number, min_val, max_val))
+                current_attempts = test_kernel(number, min_val, \
+                    max_val)
+                # Check result correctness
+                try:
+                    if current_attempts:
+                        attempts_numbers.append(current_attempts)
+                    else:
+                        raise ValueError('Get 0 or None')
+                except ValueError as e:
+                    print(e)
+                    return          
             score = int(np.mean(attempts_numbers))
             
-            print(f'{test_kernel.__name__} guesses {num_type_str} in range [{min_val}, {max_val}]{res_type_str}in {score} attempts. Total count of tests: {test_number}')
+            print(f'{test_kernel.__name__} guesses {num_type_str} ' 
+                  + f'in range [{min_val}, {max_val}]{res_type_str}in ' 
+                  + f'{score} attempts. Total count of tests: {test_number}')
             
             return score
         
@@ -85,14 +101,14 @@ def score_game(test_number:int=1) -> callable:
     return test_kernel_decorator
 
 
-@score_game(test_number)
 def random_predict(guess_number:int=1, min_val:int=1, max_val:int=100) -> int:
     """Random predicting the number
 
     Args:
         guess_number (int, optional): Guess number. Defaults to 1.
         min_val (int, optional): Minimum value of guess number. Defaults to 1.
-        max_val (int, optional): Maximum value of guess number. Defaults to 100.
+        max_val (int, optional): Maximum value of guess number. 
+            Defaults to 100.
 
     Returns:
         int: Number of attempts
@@ -110,20 +126,22 @@ def random_predict(guess_number:int=1, min_val:int=1, max_val:int=100) -> int:
         
     while True:
         count += 1
-        predict_number = np.random.randint(min_val, max_val+1) # Guessed number
+        predict_number = np.random.randint(min_val, max_val+1)
         if predict_number == guess_number:
             break # Exit, if we guess
     return(count)
 
 
-@score_game(test_number)
-def random_predict_range_dividing(guess_number:int=1, min_val:int=1, max_val:int=100) -> int:
-    """Random predicting the number with dividing search range according to more/less fedback 
+def random_predict_range_dividing(guess_number:int=1, min_val:int=1, \
+    max_val:int=100) -> int:
+    """Random predicting the number with dividing search range 
+    according to more/less fedback 
 
     Args:
         guess_number (int, optional): Guess number. Defaults to 1.
         min_val (int, optional): Minimum value of guess number. Defaults to 1.
-        max_val (int, optional): Maximum value of guess number. Defaults to 100.
+        max_val (int, optional): Maximum value of guess number. 
+            Defaults to 100.
 
     Returns:
         int: Number of attempts
@@ -141,7 +159,7 @@ def random_predict_range_dividing(guess_number:int=1, min_val:int=1, max_val:int
         
     while True:
         count += 1
-        predict_number = np.random.randint(min_val, max_val+1) # Guessed number
+        predict_number = np.random.randint(min_val, max_val+1)
         if predict_number > guess_number:
             max_val = predict_number
         elif predict_number < guess_number:
@@ -151,14 +169,16 @@ def random_predict_range_dividing(guess_number:int=1, min_val:int=1, max_val:int
     return(count)
 
 
-@score_game(test_number)
-def predict_division_two(guess_number:int=1, min_val:int=1, max_val:int=100) -> int:
-    """Predicting the number by getting an average value of range, decreasing according to more/less fedback 
+def predict_division_two(guess_number:int=1, min_val:int=1, \
+    max_val:int=100) -> int:
+    """Predicting the number by getting an average value of range, 
+    decreasing according to more/less fedback 
 
     Args:
         guess_number (int, optional): Guess number. Defaults to 1.
         min_val (int, optional): Minimum value of guess number. Defaults to 1.
-        max_val (int, optional): Maximum value of guess number. Defaults to 100.
+        max_val (int, optional): Maximum value of guess number. 
+            Defaults to 100.
 
     Returns:
         int: Number of attempts
@@ -176,7 +196,7 @@ def predict_division_two(guess_number:int=1, min_val:int=1, max_val:int=100) -> 
         
     while True:
         count += 1
-        predict_number = (min_val+max_val) // 2 # Guessed number
+        predict_number = (min_val+max_val) // 2
         if predict_number > guess_number:
             max_val = predict_number - 1
         elif predict_number < guess_number:
@@ -187,6 +207,6 @@ def predict_division_two(guess_number:int=1, min_val:int=1, max_val:int=100) -> 
 
 
 if __name__ == '__main__':
-    random_predict()
-    random_predict_range_dividing()
-    predict_division_two()
+    print('random_predict:', random_predict())
+    print('random_predict_range_dividing:', random_predict_range_dividing())
+    print('predict_division_two', predict_division_two())
