9ad4ccf71fe7b663ab514bb6feb5628aa226c03c
mvulf <vulfjob@gmail.com>
2023-01-09 21:11:31 +0300

    Env by decouple

--- project_3/settings.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-dotenv_path = os.path.join(os.getcwd(), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
-else:
-    raise FileNotFoundError('Can not find the Config file')
